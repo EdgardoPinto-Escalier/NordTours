986eaae63f755c0b2068ee027d120db23d9b5a32
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-07-25 22:48:12 +0200

    Created the first GET request to get all tours

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+const fs = require('fs');
 const express = require('express');
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from the server :)', app: 'NordTours' });
+  // GET request to get all the tours
+  const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/app-data/tours-basic.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours
+    }
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint now');
-});
+
 
 const port = 3000;
 app.listen(port, () => {
